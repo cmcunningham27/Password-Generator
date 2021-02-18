916707d1b323d554b2098efa53802322cde8338e
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-02-18 11:36:29 -0800

    Updated links on HTML to CSS and JS files; declared speacialCharacters variable; begun generatePassword function with a prompt and confirms of different criteria they could apply to their password that is created.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Special characters options created
+const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +15,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompts presented after clicking generate password
+function generatePassword() {
+    var passwordLength = prompt("Please enter the number of characters you want in your password. It must have at least 8 and no more than 128.");
+
+    var upperCases = confirm("Click OK if you want upper case letters in your password?");
+
+    var lowerCases = confirm("Click OK if you want lower case letters in your password?");
+
+    var numbers = confirm("ClickOK if you want numbers in your password?");
+
+    var special = confirm("Click OK if you want special characters in your password?");
+
+    
+}
+
+generatePassword();
