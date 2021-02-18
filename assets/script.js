b7f9a5b26274aee4003441e4d8132e06fbdcf344
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-02-18 14:38:37 -0800

    declared variable for character types; made sure users chracter type inserts are turned to all lower case letters; created a minimum count and minimums for all character types chosen by user; created if else if statement for each character type the user chose and have it using the minimums as well as adding to the minimum count.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
 var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+var character = ["upper case", "lower case", "numbers", "special characters"];
 
 // Write password to the #password input
 function writePassword() {
@@ -20,8 +24,16 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
     var passwordLength = prompt("Please enter the number of characters you want in your password. It must have at least 8 and no more than 128.");
 
-    var characterTypes = prompt("Please enter each caracter type you would like in your password (upper case, lower case, numeric, special characters).");
+    var characterTypes = prompt("Please enter each caracter type you would like in your password (upper case, lower case, numbers, special characters).");
 
+    characterTypes = characterTypes.toLowerCase();
+
+    var minimumCount = 0;
+
+    var minimumUpper = " ";
+    var minimumLower = " ";
+    var miniimumNumbers = " ";
+    var minimumSpecial = " ";
    
 
     //To generate criteria chosen
@@ -42,7 +54,23 @@ function generatePassword() {
       getSpecialCharacters: function() {
         return String.fromCharCode(Math.floor(Math.random() * specialCharacters.length));
       }
+      
     };
+    
+    if (characterTypes === "upper case") {
+      minimumUpper = functionArray.getUpperCase();
+      minimumCount++;
+    } else if (characterTypes === "lower case") {
+      minimumLower = functoniArray.getLowerCase();
+      minimumCount++;
+    } else if (characterTypes === "numbers") {
+      minimumNumbers = functionArray.getNumbers();
+      minimumCount++;
+    } else {
+      minimumSpeacial = functonArray.getSpecialCharacters();
+      minimumCount++;
+    };
+
 
 
 
